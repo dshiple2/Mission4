1221632adf5ded6fb1d7295df6acf5bdd1ef8c0c
dshiple2 <90435108+dshiple2@users.noreply.github.com>
2022-01-24 22:45:36 -0700

    all

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4Jan24.Models;
+
+namespace Mission4Jan24.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext blahContext { get; set; }
+        
+
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(ApplicationResponse ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission4Jan24.Models
+{
+    public class MovieContext : DbContext
+    {
+        //context
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+            // blank for now
+        }
+        public DbSet<ApplicationResponse> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+
+                new ApplicationResponse
+                {
+                    ApplicationId = 1,
+                    Category = "Family",
+                    Title = "Surfs Up",
+                    Year = 2005,
+                    Director = "Hilton",
+                    Rating = "R"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 2,
+                    Category = "Family",
+                    Title = "Megamind",
+                    Year = 2006,
+                    Director = "Washington",
+                    Rating = "G"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 3,
+                    Category = "Family",
+                    Title = "Shrek",
+                    Year = 2002,
+                    Director = "Nieldman",
+                    Rating = "PG"
+                }
+            );
+        }
+    }
+}
